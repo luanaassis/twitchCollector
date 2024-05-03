5e13b8666f0a1e84b1832618a12ef140f2de2b5c
Luana Assis <luanaassis0908@gmail.com>
2024-05-03 10:44:52 -0300

    Creating new requests

--- classes/game.py
+++ classes/game.py
@@ -1,5 +1,7 @@
 class Game:
-    def __init__(self, id, name, igdbid):
+    def __init__(self, id, name, igdbid, isTopGame, dateTop):
         self.id = id
         self.name = name
         self.igdbid = igdbid
+        self.isTopGame = isTopGame
+        self.dateTop = dateTop
--- collector.py
+++ collector.py
@@ -1,6 +1,10 @@
 import requests
+import datetime
 from classes.channel import Channel
 from classes.stream import Stream
+from classes.game import Game
+from classes.category import Category
+from classes.user import User
 
 client_id = ''
 client_secret = ''
@@ -30,7 +34,8 @@ def searchChannels(query):
     params = {'query': query}
     response = twitchApiRequestBase(endpoint, params)
     channel_id = response['data'][0]['id']
-    return(channel_id)
+    login = response['data'][0]['broadcaster_login']
+    return(channel_id,login)
 
 def getChannelInfo(broadcaster_id):
     endpoint = 'channels'
@@ -42,7 +47,7 @@ def getChannelInfo(broadcaster_id):
                          channel_info['content_classification_labels'], channel_info['is_branded_content'])
     print(newChannel.channel_name,newChannel.stream_tags,newChannel.classification_labels,newChannel.is_branded_content)
 
-def getStream(id):
+def getStreams(id):
     endpoint = 'channels'
     params = {'user_id': id}
     response = twitchApiRequestBase(endpoint, params)
@@ -52,6 +57,41 @@ def getStream(id):
                     stream_info['tags'],stream_info['type'], stream_info['viewer_count'],stream_info['is_mature'])
     print(newStream.game_name, newStream.stream_title, newStream.is_mature, newStream.stream_tags, newStream.viewer_count)
 
-id = searchChannels('gaules')
-getChannelInfo(id)
-getStream(id)
+
+def getTopGames():
+    endpoint = 'games/top'
+    response = twitchApiRequestBase(endpoint, None)
+    game_info_qty = len(response['data'])
+    for i in range(game_info_qty):
+        game_info = response['data'][i]
+        newGame = Game(game_info['id'],game_info['name'],game_info['igdb_id'],True,datetime.datetime.now())
+        print(newGame.name,newGame.isTopGame,newGame.dateTop)
+
+def getGame(id):
+    endpoint = 'games'
+    params = {'id': id}
+    response = twitchApiRequestBase(endpoint, params)
+    game_info= response['data'][0]
+    newGame = Game(game_info['id'],game_info['name'],game_info['igdb_id'],False,None)
+    print(newGame.name,newGame.isTopGame,newGame.dateTop)
+
+def getCategories(query):
+    endpoint = 'search/categories'
+    params = {'query': query}
+    response = twitchApiRequestBase(endpoint, params)
+    category_info_qty = len(response['data'])
+    for i in range(category_info_qty):
+        category_info = response['data'][i]
+        newCategory = Category(category_info['id'],category_info['name'])
+        print(newCategory.id, newCategory.name)
+
+def getUser(id, login):
+    endpoint = 'users'
+    params = {'id': id, 'login': login}
+    response = twitchApiRequestBase(endpoint, params)
+    user_info_qty = len(response['data'])
+    for i in range(user_info_qty):
+        user_info = response['data'][i]
+        newUser = User(user_info['id'],user_info['login'],user_info['display_name'],user_info['type'],
+                       user_info['broadcaster_type'], user_info['description'],user_info['created_at'])
+        print(newUser.user_name,newUser.broadcast_contract_type,newUser.channel_description,newUser.created_at)
