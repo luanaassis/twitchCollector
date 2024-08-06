42504d84d572dd4391bcb56e86215e745f71fafd
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2024-08-06 16:50:32 -0300

    Update tagScript.py

--- tagScript.py
+++ tagScript.py
@@ -15,21 +15,22 @@ for file in files:
 
 df_final = pd.concat(dataframes, ignore_index=True)
 
-count_mature_channels = df_final['Classification Labels'].notnull().sum()
-count_mature_games = df_final['Is Mature'].sum()
-most_played_games = df_final['Game Name'].value_counts()
-top_viewers = df.nlargest(10, 'Viewer Count')
-count_eighteen = df_final['Age Rating'].str.contains(pattern, case=False, na=False).sum()
+count_mature_channels_by_tag = df_final['Classification Labels'].notnull().sum()
+total_count_mature_channels = df_final[df_final['Classification Labels'].notnull()].groupby('Column_Name').size()
+total_count_mature_games = df_final['Is Mature'].sum()
+total_most_played_games = df_final['Game Name'].value_counts()
+total_top_viewers = df.nlargest(10, 'Viewer Count')
+total_count_eighteen = df_final['Age Rating'].str.contains(pattern, case=False, na=False).sum()
 
 output_file = f'C:\\Users\\luana\\Desktop\\twitchCollector-dev\\output_tag_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
 
 with open(output_file, 'w', encoding='utf-8') as f:
-    f.write(f'Count of Mature Channels: {count_mature_channels}\n')
-    f.write(f'Count of Mature Games: {count_mature_games}\n')
+    f.write(f'Count of Mature Channels: {total_count_mature_channels}\n')
+    f.write(f'Count of Mature Games: {total_count_mature_games}\n')
     f.write('Most Played Games:\n')
-    f.write(most_played_games.to_string())
+    f.write(total_most_played_games.to_string())
     f.write('\n')
     f.write('Top Viewers:\n')
-    f.write(top_viewers.to_string())
+    f.write(total_top_viewers.to_string())
     f.write('\n')
-    f.write(f'Count of Age Rating "eighteen", "18" or "CERO_Z": {count_eighteen}\n')
+    f.write(f'Count of Age Rating "eighteen", "18" or "CERO_Z": {total_count_eighteen}\n')
