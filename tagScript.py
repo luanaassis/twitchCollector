051a168268d84cebf4a7896634461082efa0628b
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2024-08-07 22:09:54 -0300

    Update tagScript.py

--- tagScript.py
+++ tagScript.py
@@ -13,50 +13,43 @@ for file in files:
     df = pd.read_excel(file_path, engine='openpyxl')
     dataframes.append(df)
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.expand_frame_repr', False)
 df_mid = pd.concat(dataframes, ignore_index=True)
 df_final = df.drop_duplicates(subset=['Channel Id', 'Stream Title', 'Game ID'], keep='last')
 
-total_count_mature_channels = df_final['Classification Labels'].notnull().sum()
+
+#Metrics
+unique_channels = df_final['Channel Id'].nunique()
+unique_games = df_final['Game Name'].nunique()
 count_mature_channels_by_tag = df_final[df_final['Classification Labels'].notnull()].groupby('Source URL').size()
-total_count_mature_games = df_final['Is Mature'].sum()
 count_mature_games_by_tag = df_final.groupby('Source URL')['Is Mature'].sum()
-total_most_played_games = df_final['Game Name'].value_counts()
 most_played_games_by_tag = df_final.groupby('Source URL')['Game Name'].value_counts()
-total_top_viewers = df_final.nlargest(10, 'Viewer Count')
-top_viewers_by_tag = df_final.groupby('Source URL').apply(lambda x: x.nlargest(10, 'Viewer Count')).reset_index(drop=True)
-total_count_eighteen = df_final['Age Rating'].str.contains(pattern, case=False, na=False).sum()
+most_played_games_by_tag = most_played_games_by_tag.reset_index(name='Count')
+result_most_played_games_by_tag = pd.merge(most_played_games_by_tag, df_final[['Source URL', 'Game Name', 'Age Rating']], on=['Source URL', 'Game Name'], how='left')
+top_viewers_by_tag = df_final.groupby('Source URL').apply(lambda x: x.nlargest(20, 'Viewer Count')).reset_index(drop=True)
+colunas_desejadas = ['Source URL', 'Game Name', 'Viewer Count', 'Age Rating','Channel Name']
+top_viewers_by_tag = top_viewers_by_tag[colunas_desejadas]
 count_eighteen_by_tag = df_final[df_final['Age Rating'].str.contains(pattern, case=False, na=False)].groupby('Source URL').size()
+
 output_file = f'C:\\Users\\luana\\Desktop\\twitchCollector-dev\\output_tag_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
 
 with open(output_file, 'w', encoding='utf-8') as f:
-    # Total e proporção de canais maduros
-    f.write(f"Total count of mature channels: {total_count_mature_channels}\n")
+    f.write('Mature channels found:\n' )
     for tag, count in count_mature_channels_by_tag.items():
-        proportion = (count / total_count_mature_channels) * 100
-        f.write(f"Tag: {tag}, Count: {count}, Proportion: {proportion:.2f}%\n")
-
-    # Total e proporção de jogos maduros
-    f.write(f"\nTotal count of mature games: {total_count_mature_games}\n")
+        proportion = (count / unique_channels) * 100
+        f.write(f"Tag: {tag}, Count: {count}, Total:{unique_channels}, Proportion: {proportion:.2f}%\n")
+    f.write('Defined as mature games by streamer found:\n')
     for tag, count in count_mature_games_by_tag.items():
-        proportion = (count / total_count_mature_games) * 100
-        f.write(f"Tag: {tag}, Count: {count}, Proportion: {proportion:.2f}%\n")
-
-    # Total e proporção dos jogos mais jogados
-    f.write(f"\nTotal most played games:\n{total_most_played_games}\n")
-    for tag, counts in most_played_games_by_tag.groupby('Source URL'):
-        f.write(f"\nTag: {tag}\n")
-        f.write(f"{counts}\n")
-        # Proporção não calculada aqui pois não há um total consolidado único
-
-    # Total e proporção dos top visualizadores
-    f.write(f"\nTotal top viewers:\n{total_top_viewers}\n")
-    for tag, top_viewers in top_viewers_by_tag.groupby('Source URL'):
-        f.write(f"\nTag: {tag}\n")
-        f.write(f"{top_viewers}\n")
-        # Proporção não calculada aqui pois não há um total consolidado único
-
-    # Total e proporção de jogos com classificação 18
-    f.write(f"\nTotal count of games rated 18: {total_count_eighteen}\n")
+        proportion = (count / unique_games) * 100
+        f.write(f"Tag: {tag}, Count: {count}, Total:{unique_games},Proportion: {proportion:.2f}%\n")
+    f.write('Mature games found:\n')
     for tag, count in count_eighteen_by_tag.items():
-        proportion = (count / total_count_eighteen) * 100
-        f.write(f"Tag: {tag}, Count: {count}, Proportion: {proportion:.2f}%\n")
+        proportion = (count / unique_games) * 100
+        f.write(f"Tag: {tag}, Count: {count}, Total:{unique_games}, Proportion: {proportion:.2f}%\n")
+    f.write('Most played games by tag:\n')
+    f.write(f"{result_most_played_games_by_tag}\n")
+    f.write('Most viewed lives by tag:\n')
+    f.write(f"\n{top_viewers_by_tag}\n")
