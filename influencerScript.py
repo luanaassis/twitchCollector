94a9f51f5ac6dfb1bfdb3661bd5b858b32560421
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2024-08-15 19:32:51 -0300

    Update influencerScript.py

--- influencerScript.py
+++ influencerScript.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-import datetime
+import matplotlib.pyplot as plt
 
 folder = 'C:\\Users\\luana\\Desktop\\twitchCollector-dev'
 files = [f for f in os.listdir(folder) if f.endswith('.xlsx') and f.startswith('twitch_data_influencers')]
@@ -45,59 +45,79 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.expand_frame_repr', False)
 df_mid = pd.concat(dataframes, ignore_index=True)
-df_final = df.drop_duplicates(subset=['Channel Id', 'Stream/Video Title', 'Game ID'], keep='last')
+df_final = df_mid.drop_duplicates(subset=['Channel Id','Channel Name','Stream/Video Title', 'Game ID'])
+df_final = df_final[df_final['Channel Name'].isin(isAdult.keys())]
 
 #Metrics
-unique_channels = df_final['Channel Id'].nunique()
-unique_games = df_final['Game Name'].nunique()
-unique_videos_lives = df_final['Stream/Video Title'].nunique()
 
-count_eighteen_by_influencer = df_final[df_final['Age Rating'].str.contains(patternInapropriate, case=False, na=False)].groupby('Channel Name').size()
+count_eighteengames_by_influencer = df_final[df_final['Age Rating'].str.contains(patternInapropriate, case=False, na=False)].groupby('Channel Name').size()
+count_NOT_eighteengames_by_influencer = df_final[~df_final['Age Rating'].str.contains(patternInapropriate, case=False, na=False)].groupby('Channel Name').size()
+
 count_mature_channels_by_influencer = df_final[df_final['Classification Labels'].notnull()].groupby('Channel Name').size()
+
+
 count_mature_games_defined_by_influencer = df_final.groupby('Channel Name')['Is Mature'].sum()
-filtered_results_count_mature_games_defined_by_influencer = count_mature_games_defined_by_influencer[count_mature_games_defined_by_influencer != 0]
+
 top_viewers_by_influencer = df_final.groupby('Channel Name').apply(lambda x: x.nlargest(20, 'Viewer Count')).reset_index(drop=True)
 colunas_desejadas = ['Game Name', 'Viewer Count', 'Age Rating','Channel Name', 'Stream/Video Title']
 top_viewers_by_influencer = top_viewers_by_influencer[colunas_desejadas]
 top_viewers_by_influencer['Age Rating Flag'] = top_viewers_by_influencer['Age Rating'].str.contains(patternInapropriate).astype(float)
-
+counts_top_viewers_by_influencer = top_viewers_by_influencer.groupby('Channel Name')['Age Rating Flag'].value_counts().unstack(fill_value=0)
 
 isAdult = pd.DataFrame(list(isAdult.items()), columns=['Channel Name', 'Is Adult'])
 isAdultMerged = pd.merge(df_final, isAdult, on='Channel Name')
 isAdultFiltered = isAdultMerged[isAdultMerged['Is Adult'] == 1]
 isKidFiltered = isAdultMerged[isAdultMerged['Is Adult'] == 0]
 
-unique_games_streamedByAdults = isAdultFiltered['Game Name'].nunique()
-unique_games_streamedByKids = isKidFiltered['Game Name'].nunique()
-
 games_innapropriates_streamedByAdults = isAdultFiltered[isAdultFiltered['Age Rating'].str.contains(patternInapropriate, case=False, na=False)].groupby('Channel Name').size()
 games_innapropriates_streamedByKids = isKidFiltered[isKidFiltered['Age Rating'].str.contains(patternInapropriate, case=False, na=False)].groupby('Channel Name').size()
 
-output_file = f'C:\\Users\\luana\\Desktop\\twitchCollector-dev\\output_influencers_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
+#First graph
+consolidated_counts = pd.DataFrame({
+    '18+ Games': count_eighteengames_by_influencer,
+    'Non-18+ Games': count_NOT_eighteengames_by_influencer
+}).fillna(0)
+num_channels = len(consolidated_counts)
+fig, axes = plt.subplots(num_channels, 1, figsize=(8, 4 * num_channels))
+for (channel, counts), ax in zip(consolidated_counts.iterrows(), axes):
+    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=140, colors=['red', 'green'])
+    ax.set_title(f'Distribuição de Jogos por Classificação Etária para {channel}')
 
-with open(output_file, 'w', encoding='utf-8') as f:
-    f.write('Existe diferença na classificação de jogos exibidos por influenciadores infantis com - de 18 anos com influenciadores infantis que possuem + de 18 anos?\n' )
-    f.write('Influenciadores infantis com - de 18 anos:\n')
-    for influencer, count in games_innapropriates_streamedByKids.items():
-        proportion = (count / unique_games_streamedByKids) * 100
-        f.write(f"Influencer: {influencer}, Count: {count}, Total:{unique_games_streamedByKids}, Proportion: {proportion:.2f}%\n")
-    f.write('Influenciadores infantis com + de 18 anos:\n')
-    for influencer, count in games_innapropriates_streamedByAdults.items():
-        proportion = (count / unique_games_streamedByAdults) * 100
-        f.write(f"Influencer: {influencer}, Count: {count}, Total:{unique_games_streamedByAdults}, Proportion: {proportion:.2f}%\n")
-    f.write('Os jogos de influenciadores para o publico infantil sao apropriados para este publico?\n' )
-    for influencer, count in count_eighteen_by_influencer.items():
-        proportion = (count / unique_games) * 100
-        f.write(f"Influencer: {influencer}, Count: {count}, Total:{unique_games}, Proportion: {proportion:.2f}%\n")
-    f.write('Os conteúdos mais visualizados dentre estes influenciadores são de conteúdos apropriados ao público infantil?\n' )
-    f.write(f"\n{top_viewers_by_influencer}\n")
-    f.write('A classificação do canal e dos jogos foram devidamente indicados no canal destes influenciadores?\n' )
-    f.write('Canais marcados como adultos:\n')
-    for influencer, count in count_mature_channels_by_influencer.items():
-        proportion = (count / unique_channels) * 100
-        f.write(f"Influencer: {influencer}, Count: {count}, Total:{unique_channels}, Proportion: {proportion:.2f}%\n")
-    f.write('Canais com jogos marcados como adultos: \n')
-    for influencer, count in filtered_results_count_mature_games_defined_by_influencer.items():
-        proportion = (count / unique_games) * 100
-        f.write(f"Influencer: {influencer}, Count: {count}, Total:{unique_games}, Proportion: {proportion:.2f}%\n")
-    
+plt.tight_layout()
+plt.show()
+#Second graph
+count_mature_channels_by_influencer.plot(kind='bar', color='red')
+plt.title('Número de Canais com Classificação Etária Imprópria por Influenciador')
+plt.xlabel('Canal')
+plt.ylabel('Número de Canais com Classificação Etária')
+plt.show()
+#Third graph
+count_mature_games_defined_by_influencer.plot(kind='bar', color='red')
+plt.title('Canais com Classificação Adulta por Influenciador')
+plt.xlabel('Canal')
+plt.ylabel('Canais com Classificação Adulta')
+plt.show()
+#Fourth graph
+num_channels = len(counts_top_viewers_by_influencer)
+fig, axes = plt.subplots(num_channels, 1, figsize=(8, 4 * num_channels))
+if num_channels == 1:
+    axes = [axes]
+for (channel, counts), ax in zip(counts_top_viewers_by_influencer.iterrows(), axes):
+    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=140, colors=['green','red'])
+    ax.set_title(f'Distribuição de Indicadores de Classificação dos Videos/Streams mais Visualizadas para {channel}')
+plt.tight_layout()
+plt.show()
+#Fifth graph
+if not games_innapropriates_streamedByAdults.empty:
+    games_innapropriates_streamedByAdults.plot(kind='bar', color='red')
+    plt.title('Quantidade de Jogos Inapropriados Transmitidos por Influencers Adultos')
+    plt.xlabel('Canal')
+    plt.ylabel('Número de Jogos Inapropriados')
+    plt.show()
+
+if not games_innapropriates_streamedByKids.empty:
+    games_innapropriates_streamedByKids.plot(kind='bar', color='red')
+    plt.title('Quantidade de Jogos Inapropriados Transmitidos por Influencers Crianças')
+    plt.xlabel('Canal')
+    plt.ylabel('Número de Jogos Inapropriados')
+    plt.show()
